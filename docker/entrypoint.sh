5465343c916da6cb18f96ced0c24a8e0c291613e
amarees <saleh.amareen@dominos.com>
2025-04-10 10:51:40 -0400

    Added the proper directory

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,14 +4,14 @@ set -e
 cd /repo/dists/stable/main/binary-all
 
 echo "📦 Generating APT index for all..."
-dpkg-scanpackages . /dev/null > Packages
+dpkg-scanpackages . /dev/null | sed 's|^Filename: ./|Filename: dists/stable/main/binary-all/|' > Packages
 gzip -9c Packages > Packages.gz
 echo "✅ APT index generated:"
 ls -lh Packages*
 
 cd /repo/dists/stable/main/binary-arm64
 echo "📦 Generating APT index for arm64..."
-dpkg-scanpackages . /dev/null > Packages
+dpkg-scanpackages . /dev/null | sed 's|^Filename: ./|Filename: dists/stable/main/binary-arm64/|' > Packages
 gzip -9c Packages > Packages.gz
 
 echo "✅ APT index generated:"
