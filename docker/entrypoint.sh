55289057c36f51e6757cd1c0d5a2ce9f6af96e88
amarees <saleh.amareen@dominos.com>
2025-04-10 10:31:13 -0400

    Added packages for arm64

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,7 +3,14 @@ set -e
 
 cd /repo/dists/stable/main/binary-all
 
-echo "📦 Generating APT index..."
+echo "📦 Generating APT index for all..."
+dpkg-scanpackages . /dev/null > Packages
+gzip -9c Packages > Packages.gz
+echo "✅ APT index generated:"
+ls -lh Packages*
+
+cd /repo/dists/stable/main/binary-arm64
+echo "📦 Generating APT index for arm64..."
 dpkg-scanpackages . /dev/null > Packages
 gzip -9c Packages > Packages.gz
 
